efec233a6545d4115b79fc9962b65aec2fbae925
Survila-Dev <65458205+Survila-Dev@users.noreply.github.com>
2023-02-21 07:21:57 +0100

    add redux template

--- Frontend/vis-prog/src/App.tsx
+++ Frontend/vis-prog/src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
-import { useSelector, useDispatch } from "react-redux";
+import { actions, useAppDispatch, useAppSelector, changeAsync } from "./store/index";
 import './App.css';
 
 function App() {
+
+  const count = useAppSelector((state) => state.counterRed.counter)
+  const dispatch = useAppDispatch();
+
+  const handleClick = (e: React.FormEvent) => {
+    dispatch(actions.change(12))
+  }
+
+  const handleAsyncClick = (e: React.FormEvent) => {
+    dispatch(changeAsync(20)) 
+  }
+
   return (
     <div className="App">
+      <p>{count}</p>  
+      <button onClick = {handleClick}>Click me</button>
+      <button onClick = {handleAsyncClick}>Click me</button>
     </div>
   );
 }
--- Frontend/vis-prog/src/store/index.tsx
+++ Frontend/vis-prog/src/store/index.tsx
@@ -1,18 +1,64 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, configureStore, createSlice } from "@reduxjs/toolkit"
 import type { PayloadAction } from '@reduxjs/toolkit'
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
+
+export interface CounterState {
+    counter: number,
+    status: "idle" | "loading" | "failed",
+}
+
+const initCounterStateValue: CounterState = {
+    counter: 0,
+    status: "idle",
+}
+
+// A mock function to mimic making an async request for data
+function fetchCount(amount = 1) {
+    return new Promise<{ data: number }>((resolve) =>
+      setTimeout(() => resolve({ data: amount }), 2000)
+    );
+  }
+
+export const changeAsync = createAsyncThunk(
+    "counter/asyncCounter",
+    async (changeValue: number) => {
+        const res = await fetchCount(changeValue)
+        return res.data
+    }
+)
 
 const counterSlice = createSlice({
     name: "counter",
-    initialState: { counter: 0 },
+    initialState: initCounterStateValue,
     reducers: {
         change(state, action: PayloadAction<number>) {
             state.counter = action.payload;
         }
+    },
+
+    extraReducers: (builder) => {
+        builder
+            .addCase(changeAsync.pending, (state) => {
+                state.status = "loading"
+            })
+            .addCase(changeAsync.fulfilled, (state, action) => {
+                state.status = "idle"
+                state.counter += action.payload
+            })
+            .addCase(changeAsync.rejected, (state) => {
+                state.status = "failed"
+            })
     }
 })
 
 export const store = configureStore({
     reducer: {counterRed: counterSlice.reducer}
 })
+
+export type DispatchType = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
+
+export const useAppDispatch = useDispatch<DispatchType>;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export const actions = counterSlice.actions;
