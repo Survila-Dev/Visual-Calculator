fca566fc34db6861cd916f0e5a8360b50918bda1
Survila-Dev <65458205+Survila-Dev@users.noreply.github.com>
2023-02-24 21:43:32 +0100

    clean up in controlbar component

--- Frontend/vis-prog/src/components/controlbar.tsx
+++ Frontend/vis-prog/src/components/controlbar.tsx
@@ -4,17 +4,9 @@ import React from "react"
 
 export const ControlBar: React.FC = () => {
 
-    const marginWidth = "4"
-    // let classNameInput: string = "absolute flex-none mx-["+marginWidth+"vw] w-[90vw] bg-red-" + 200;
-    const classNameInput: string = 'absolute flex-none mx-['+'10vw'+'] w-[90vw] bg-red-'+'100';
-    const classNameInput2: string = 'absolute flex-none mx-[5vw] w-[90vw] bg-red-200';
-
-    const classNameInput3 = classNameInput;
-
     const x = "100px"
     const y = "20px"
     
-    console.log(classNameInput)
     return (
         <section className = "absolute flex-none mx-[5vw] w-[90vw] bg-red-100" style = {{top: x, left: y}}>Control bar here.</section>
         )
