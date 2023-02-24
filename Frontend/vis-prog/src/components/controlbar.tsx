845603f657cab2696895683682ffd5101fb549ed
Survila-Dev <65458205+Survila-Dev@users.noreply.github.com>
2023-02-24 07:13:29 +0100

    add positioning from variables

--- Frontend/vis-prog/src/components/controlbar.tsx
+++ Frontend/vis-prog/src/components/controlbar.tsx
@@ -1,7 +1,18 @@
 import React from "react"
 
+
+
 export const ControlBar: React.FC = () => {
+
+    const marginWidth = "4"
+    // let classNameInput: string = "absolute flex-none mx-["+marginWidth+"vw] w-[90vw] bg-red-" + 200;
+    const classNameInput: string = 'absolute flex-none mx-['+'10vw'+'] w-[90vw] bg-red-'+'100';
+    const classNameInput2: string = 'absolute flex-none mx-[5vw] w-[90vw] bg-red-200';
+
+    const classNameInput3 = classNameInput;
+    
+    console.log(classNameInput)
     return (
-        <section className = "absolute flex-none mx-[5vw] w-[90vw] bg-red-100">Control bar here.</section>
+        <section className = "absolute flex-none mx-[5vw] w-[90vw] bg-red-100" style = {{top: "0px", left: "0px"}}>Control bar here.</section>
         )
 }
--- Frontend/vis-prog/src/components/workspace-field.tsx
+++ Frontend/vis-prog/src/components/workspace-field.tsx
@@ -4,8 +4,10 @@ import { ControlBar } from "../components/controlbar"
 export const WorkspaceField: React.FC = () => {
     return (
         <section className = "flex-1 bg-green-200">
-            <ControlBar/>
-            Workspace here
+            <div className = "relative">
+                <ControlBar/>
+                Workspace here
+            </div>
         </section>
     )
 }
