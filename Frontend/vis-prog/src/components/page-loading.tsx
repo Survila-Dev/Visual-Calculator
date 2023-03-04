b25bcc8d5bc60963151195fb58fb06654efcbf8e
Survila-Dev <65458205+Survila-Dev@users.noreply.github.com>
2023-03-04 11:15:17 +0100

    add spinner for page loader

--- Frontend/vis-prog/src/components/page-loading.tsx
+++ Frontend/vis-prog/src/components/page-loading.tsx
@@ -2,6 +2,9 @@ import React from "react"
 
 export const PageLoading: React.FC = () => {
     return (
-        <div>Page is loading!!!</div>
+        <div className = "flex bg-gray-700 h-screen w-screen flex-col justify-center items-center gap-5">
+            <div className = "h-40 w-40 bg-red-500"></div>
+            <div className = "text-xl text-white">Page is loading ...</div>
+        </div>
         )
 }
