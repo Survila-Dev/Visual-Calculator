964fb0c33d0450e96560ae124444dd727b7f2b1c
Survila-Dev <65458205+Survila-Dev@users.noreply.github.com>
2023-03-26 10:45:54 +0200

    work on axios requests

--- Frontend/vis-prog/src/store/workspaces-subroutines/workspaces-thunks.tsx
+++ Frontend/vis-prog/src/store/workspaces-subroutines/workspaces-thunks.tsx
@@ -12,68 +12,80 @@ export const getWorkspaceFromBackend = createAsyncThunk(
     "workspace/getWorkspaceFromBackend",
     async (input: {authToken: string}, thunkAPI) => {
 
-        console.log({'Authorization': 'Bearer ' + input.authToken})
-        const result = await axios({
-            url: BACKENDURL,
-            method: "post",
-            headers: {
-                'Authorization': 'Bearer ' + input.authToken,
-                'Content-Type': `application/json`,
-                'Accept'      : `application/json`},
-            data: {
-                query: `
-                    query {
-                        currentWorkspace {
-                            id
-                            name
-                            fieldPosition {
-                                x
-                                y
-                              }
-                            nodes {
-                            id
-                            type
-                            position {
-                                x
-                                y
-                            }
-                            connections {
-                                id
-                                nodeId
-                                portSelf
-                                otherNodeId
-                                portOther
-                            }
-                            value
-                            fullyConnected
-                            
-                            }
-                            curveConnections {
-                                firstNodeId
-                                firstPortId
-                                secondNodeId
-                                secondPortId
-                                firstPortPosition {
-                                    x
-                                    y
+        console.log("Access token:")
+        console.log(input.authToken)
+
+        try {
+            const result = await axios.post(
+                BACKENDURL,
+                {
+                    // withCredentials: false,
+                    headers: {
+                        'Authorization': 'Bearer ' + "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Il96RlZ1Uk5BVGlwdnIyYmVtOFRMdyJ9.eyJpc3MiOiJodHRwczovL2Rldi1odXFlZGdqdHViY213cGRlLnVzLmF1dGgwLmNvbS8iLCJzdWIiOiJWdGYySTNlVDhNT0NKVERzSlV3ZTB5bmx2N24yaG1PNkBjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly92aXNwcm9nLmJhY2tlbmQuY29tIiwiaWF0IjoxNjc5ODE0NTIxLCJleHAiOjE2Nzk5MDA5MjEsImF6cCI6IlZ0ZjJJM2VUOE1PQ0pURHNKVXdlMHlubHY3bjJobU82IiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.r9IGVcefTX584Xb2YR6KT94iDvbK2zKJmkSkn8gCG98Dx6QY7wSpEgIPp_p3lsuNXbPF6zuv9lS5tLuB2tMrcdA2NiE6Wl7Wf7uOmabIk7CcTQcvV3YG0QLvQqNhUgrvf5ixsWmTppXsGSrbPTZBqWT-xshesVwB2RX576dwZNMrjlLXEbtjeZ7NKkC86FgObdU-VY0v33AJFBLW3Xiy2LPmAMnQszNe1SvyCStvYmKQcJyXPMwq_ZMQfONPwJq1uVVKzxM5WE7z1NJ1KHrxa9bkZG8Gw_otEmPaMwdDI1QYiqQ5jD2LFBzYlcC8MR4mttZImVaxWtpHgfNE5_bFwg",
+                        // 'Content-Type': `application/json`,
+                        'Accept'      : `application/json`,
+                        "Access-Control-Allow-Origin": "*",
+                        
+                    },
+                    data: {
+                        query: `
+                            query {
+                                currentWorkspace {
+                                    id
+                                    name
+                                    fieldPosition {
+                                        x
+                                        y
+                                    }
+                                    nodes {
+                                    id
+                                    type
+                                    position {
+                                        x
+                                        y
+                                    }
+                                    connections {
+                                        id
+                                        nodeId
+                                        portSelf
+                                        otherNodeId
+                                        portOther
+                                    }
+                                    value
+                                    fullyConnected
+                                    
+                                    }
+                                    curveConnections {
+                                        firstNodeId
+                                        firstPortId
+                                        secondNodeId
+                                        secondPortId
+                                        firstPortPosition {
+                                            x
+                                            y
+                                        }
+                                        secondPortPosition {
+                                            x
+                                            y
+                                        }
+                                    }
+                                    
                                 }
-                                secondPortPosition {
-                                    x
-                                    y
-                                }
                             }
-                            
-                        }
+                            `
                     }
-                    `
-            }
-        })
+            })
+            const newWorkspace = result.data.data.currentWorkspace
+            newWorkspace.initNodes = dropDownNodes
+            const curConnections = JSON.parse(JSON.stringify(newWorkspace.curveConnections))
+            delete newWorkspace.curveConnections
+            return {workspace: newWorkspace, curveConnections: curConnections}
+
+        } catch (e) {
+            console.error(e)
+            return {workspace: {}, curveConnections: []}
+        }
         
-        const newWorkspace = result.data.data.currentWorkspace
-        newWorkspace.initNodes = dropDownNodes
-        const curConnections = JSON.parse(JSON.stringify(newWorkspace.curveConnections))
-        delete newWorkspace.curveConnections
-        return {workspace: newWorkspace, curveConnections: curConnections}
     }
 )
 
