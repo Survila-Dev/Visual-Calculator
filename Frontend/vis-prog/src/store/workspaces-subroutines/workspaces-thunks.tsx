90c7706f9c8287f8eb8cdd34e201246b5e4be99e
Survila-Dev <65458205+Survila-Dev@users.noreply.github.com>
2023-04-01 08:12:24 +0200

    working auth

--- Frontend/vis-prog/src/store/workspaces-subroutines/workspaces-thunks.tsx
+++ Frontend/vis-prog/src/store/workspaces-subroutines/workspaces-thunks.tsx
@@ -87,66 +87,64 @@ export const getWorkspaceFromBackend = createAsyncThunk(
             //         `
             //     }
             // )
-            // console.log("Fetch successful.")
+            console.log("Fetch successful.")
 
-            const result = await axios.post(
-                BACKENDURL,
-                {
-                    // withCredentials: false,
-                    // headers: {
-                    //     Authorization: 'Bearer ' + input.authToken, //"eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Il96RlZ1Uk5BVGlwdnIyYmVtOFRMdyJ9.eyJpc3MiOiJodHRwczovL2Rldi1odXFlZGdqdHViY213cGRlLnVzLmF1dGgwLmNvbS8iLCJzdWIiOiJWdGYySTNlVDhNT0NKVERzSlV3ZTB5bmx2N24yaG1PNkBjbGllbnRzIiwiYXVkIjoiaHR0cHM6Ly92aXNwcm9nLmJhY2tlbmQuY29tIiwiaWF0IjoxNjc5ODE0NTIxLCJleHAiOjE2Nzk5MDA5MjEsImF6cCI6IlZ0ZjJJM2VUOE1PQ0pURHNKVXdlMHlubHY3bjJobU82IiwiZ3R5IjoiY2xpZW50LWNyZWRlbnRpYWxzIn0.r9IGVcefTX584Xb2YR6KT94iDvbK2zKJmkSkn8gCG98Dx6QY7wSpEgIPp_p3lsuNXbPF6zuv9lS5tLuB2tMrcdA2NiE6Wl7Wf7uOmabIk7CcTQcvV3YG0QLvQqNhUgrvf5ixsWmTppXsGSrbPTZBqWT-xshesVwB2RX576dwZNMrjlLXEbtjeZ7NKkC86FgObdU-VY0v33AJFBLW3Xiy2LPmAMnQszNe1SvyCStvYmKQcJyXPMwq_ZMQfONPwJq1uVVKzxM5WE7z1NJ1KHrxa9bkZG8Gw_otEmPaMwdDI1QYiqQ5jD2LFBzYlcC8MR4mttZImVaxWtpHgfNE5_bFwg",
-                    //     'Content-Type': `application/json`,
-                    //     'Accept'      : `application/json`,
-                    //     "Access-Control-Allow-Origin": "*",
-                        
-                    // },
-                    data: {
-                        query: `
-                            query {
-                                currentWorkspace {
-                                    id
-                                    name
-                                    fieldPosition {
-                                        x
-                                        y
-                                    }
-                                    nodes {
-                                    id
-                                    type
-                                    position {
-                                        x
-                                        y
-                                    }
-                                    connections {
-                                        id
-                                        nodeId
-                                        portSelf
-                                        otherNodeId
-                                        portOther
-                                    }
-                                    value
-                                    fullyConnected
-                                    
-                                    }
-                                    curveConnections {
-                                        firstNodeId
-                                        firstPortId
-                                        secondNodeId
-                                        secondPortId
-                                        firstPortPosition {
-                                            x
-                                            y
-                                        }
-                                        secondPortPosition {
-                                            x
-                                            y
-                                        }
-                                    }
-                                    
+            const result = await axios({
+                method: "post",
+                url: BACKENDURL,
+                headers: {
+                    'Authorization': 'Bearer ' + input.authToken,
+                    'Content-Type': `application/json`,
+                    'Accept'      : `application/json`,
+                    // "Access-Control-Allow-Origin": "*",
+                },
+                data: {
+                    query: `
+                        query {
+                            currentWorkspace {
+                                id
+                                name
+                                fieldPosition {
+                                    x
+                                    y
                                 }
+                                nodes {
+                                id
+                                type
+                                position {
+                                    x
+                                    y
+                                }
+                                connections {
+                                    id
+                                    nodeId
+                                    portSelf
+                                    otherNodeId
+                                    portOther
+                                }
+                                value
+                                fullyConnected
+                                
+                                }
+                                curveConnections {
+                                    firstNodeId
+                                    firstPortId
+                                    secondNodeId
+                                    secondPortId
+                                    firstPortPosition {
+                                        x
+                                        y
+                                    }
+                                    secondPortPosition {
+                                        x
+                                        y
+                                    }
+                                }
+                                
                             }
-                            `
-                    }
+                        }
+                        `
+                }
             })
             const newWorkspace = result.data.data.currentWorkspace
             newWorkspace.initNodes = dropDownNodes
@@ -207,13 +205,13 @@ export const uploadWorkspaceToBackend = createAsyncThunk(
         }
 
         const result = await axios({
-            url: BACKENDURL,
             method: "post",
+            url: BACKENDURL,
             headers: {
                 'Authorization': 'Bearer ' + input.authToken,
                 'Content-Type': `application/json`,
                 'Accept'      : `application/json`,
-                "Access-Control-Allow-Origin": "*",
+                // "Access-Control-Allow-Origin": "*",
             },
             data: {
                 query: `
