a28dbdfa95294ef0dd4235ffb93709d1fcb99dc0
Survila-Dev <65458205+Survila-Dev@users.noreply.github.com>
2023-04-17 07:20:22 +0200

    add favicon

--- Frontend/vis-prog/src/components/footer.tsx
+++ Frontend/vis-prog/src/components/footer.tsx
@@ -2,8 +2,9 @@ import React from "react"
 
 export const Footer: React.FC = () => {
     return (
-        <section className="flex-none z-20 px-2 pb-1 text-sm bg-black text-white min-h-0">
-            Created by Eimantas Survila, 2023
+        <section className="flex justify-between z-20 px-2 pb-1 text-sm bg-black text-white min-h-0">
+            <p>Created by Eimantas Survila, 2023</p>
+            <a href="https://www.flaticon.com/free-icons/globe" title="globe icons">Globe icons created by Prosymbols - Flaticon</a>
         </section>
         )
 }
