7b051917ab8ff5e616f921838c95f8de5a6f50bd
Survila-Dev <65458205+Survila-Dev@users.noreply.github.com>
2023-04-18 12:14:38 +0200

    remove firebase.json

--- Frontend/vis-prog/src/components/footer.tsx
+++ Frontend/vis-prog/src/components/footer.tsx
@@ -4,7 +4,7 @@ export const Footer: React.FC = () => {
     return (
         <section className="flex justify-between z-20 px-2 pb-1 text-sm bg-black text-white min-h-0">
             <p>Created by Eimantas Survila, 2023</p>
-            <a href="https://www.flaticon.com/free-icons/globe" title="globe icons">Globe icons created by Prosymbols - Flaticon</a>
+            {/* <a href="https://www.flaticon.com/free-icons/globe" title="globe icons">Globe icons created by Prosymbols - Flaticon</a> */}
         </section>
         )
 }
